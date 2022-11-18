d9d70622d026f456992892d533e3135466b48ca3
DorianFaivre <dorian.fvre70@gmail.com>
2022-11-18 15:44:24 +0100

    Squelette du projet + classes Entities et BL.Contracts réalisées

---
+++ GameApp/Game.BL.Contracts/IVideoGameService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game.Entities;
+
+namespace Game.BL.Contracts
+{
+    public interface IVideoGameService
+    {
+        public VideoGame Get(int id);
+
+        public List<VideoGame> GetAll();
+
+        public void Create(VideoGame videoGame);
+
+        public void Delete(int id);
+
+        public VideoGame Update(int id, VideoGame updateVideoGame);
+
+    }
+}
