dcdf31b015f28bdb8b4fe9a7b69f68282e82c804
DorianFaivre <dorian.fvre70@gmail.com>
2023-01-19 17:20:10 +0100

    controller

---
+++ GameApp/GameApp/Controllers/GameController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Game.BL.Contracts;
+
+namespace GameApp.Controllers
+{
+    public class GameController : Controller
+    {
+        private IVideoGameService _videoGameService;
+
+        // Conctructor
+        public GameController(IVideoGameService videoGameService)
+        {
+            _videoGameService = videoGameService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult VideoGame()
+        {
+            var games = _videoGameService.GetAll();
+            return View(games);
+        }
+    }
+}
